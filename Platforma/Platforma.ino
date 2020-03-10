fad9da0a2e6f30ff821d02e3ce2c997ba226ba5a
Kompan15 <rafal.mistela@gmail.com>
2020-03-10 23:23:19 +0100

    Rotational Matrix edited - platform is safe and working

--- Platforma/Platforma.ino
+++ Platforma/Platforma.ino
@@ -35,7 +35,7 @@ const int Y_pin = 1; // analog pin connected to Y output
 Servo servo[6];
 
 //pozycje zerowe serw
-static int zero[6]={1500,1500,1460,1460,1500,1500};
+static int zero[6]={1500,1500,1460,1460,1560,1500};
 
 //tablica przechowująca pozycję platformy
 static float arr[6]={0,0.0,0, radians(0),radians(0),radians(0)};
@@ -50,8 +50,8 @@ static int servo_pos[6];
 //rotacje ramion serwomachanizmów w stosunku do osi x
 const float beta[] = {-pi/2,pi/2,5*pi/6, -pi/6,pi/6,-5*pi/6},
 
-//maximum servo positions, 0 is horizontal position
-      servo_min=radians(-80),servo_max=radians(80),
+//maksymalne dozwolone ruchy serw - bezpieczenstwo przede wszystkim.
+      servo_min=radians(-70),servo_max=radians(70),
 
 //servo_mult - mnożnik konwertujący radiany->puls dla zadanego kąta.
 
@@ -59,25 +59,24 @@ const float beta[] = {-pi/2,pi/2,5*pi/6, -pi/6,pi/6,-5*pi/6},
 
 //z_home - height of platform above base, 0 is height of servo arms
 
-servo_mult=400/(pi/4),L1 = 16,L2 = 155, z_home = 137;
+servo_mult=400/(pi/4),L1 = 16,L2 = 155, z_home = 150;
 
-//RD distance from center of platform to attachment points (arm attachment point)
+//RD - odległość od środka platformy do snapów
+//PD  - odleglosc od srodka podstawy do miejsc zaczepienia orczykow (osie serw)
 
-//PD distance from center of base to center of servo rotation points (servo axis)
+//theta_p- kat pomiedzy wektorami wskazujacymi polozenie punktow obrotu orczykow, theta_r - kat pomiedzy punktami zaczepienia snapow na platformie
 
-//theta_p-angle between two servo axis points, theta_r - between platform attachment points
+//theta_angle- zmienna pomocnicza
 
-//theta_angle-helper variable
+//p[][]=x y koordynanty punktow rotacji
 
-//p[][]=x y values for servo rotation points
-
-//re[]{}=x y z values of platform attachment points positions
+//re[]{}=x y z koordynanty snapow platformy
 
 //equations used for p and re will affect postion of X axis, they can be changed to achieve
 
 //specific X axis position
 
-const float RD = 114, PD = 105, theta_p = radians(50),
+const float RD = 102, PD = 105, theta_p = radians(50),
 
 theta_angle=(pi/3-theta_p)/2, theta_r = radians(20),
 
@@ -127,11 +126,9 @@ theta_angle=(pi/3-theta_p)/2, theta_r = radians(20),
 
 };
 
-//arrays used for servo rotation calculation
+//tablice używane do rotacji.
 
-//H[]-center position of platform can be moved with respect to base, this is
-
-//translation vector representing this move
+//H[]-wektor translacji z podstawy do platformy.
 
 static float M[3][3], rxp[3][6], T[3], H[3] = {0,0,z_home};
 
@@ -154,17 +151,17 @@ void setup(){
 
    servo[5].attach(11, MIN, MAX);
 
-//begin of serial communication
+//rozpocznij komunikację
 
    Serial.begin(9600);
 
-//putting into base position
+//wysteruj pozycję początkową
 
    setPos(arr);
    
 }
 
-//function calculating needed servo rotation value
+//funkcja wyliczania żądanego kąta na serwomechanizmie
 
 float getAlpha(int *i){
 
@@ -184,7 +181,7 @@ float getAlpha(int *i){
 
    while(n<20){
 
-    //calculation of position of base attachment point (point on servo arm where is leg connected)
+    //obliczenia współrzędnych połączenia pomiędzy orczykami a popychaczami.
 
       q[0] = L1*cos(th)*cos(beta[*i]) + p[0][*i];
 
@@ -192,8 +189,7 @@ float getAlpha(int *i){
 
       q[2] = L1*sin(th);
 
-    //calculation of distance between according platform attachment point and base attachment point
-
+    //obliczenie dystansu pomiędzy punktami zaczepienia platformy a punktami zaczepienia podstawy
       dl[0] = rxp[0][*i] - q[0];
 
       dl[1] = rxp[1][*i] - q[1];
@@ -202,7 +198,7 @@ float getAlpha(int *i){
 
       dl2 = sqrt(dl[0]*dl[0] + dl[1]*dl[1] + dl[2]*dl[2]);
 
-    //if this distance is the same as leg length, value of theta_a is corrent, we return it
+    //jeżeli dystans d12 jest równy długości popychacza obliczenia są poprawne i można liczyć dalej.
 
       if(abs(L2-dl2)<0.01){
 
@@ -240,7 +236,7 @@ float getAlpha(int *i){
 
 
 
-//function calculating rotation matrix
+//funkcja obliczająca macierz rotacji
 
 void getmatrix(float pe[])
 
@@ -252,27 +248,27 @@ void getmatrix(float pe[])
 
    float phi=pe[3];
 
-   M[0][0] = cos(psi)*cos(theta);
+   M[0][0] = cos(psi)*cos(theta);                             //OK pierwsza kolumna pierwszy element
 
-   M[1][0] = -sin(psi)*cos(phi)+cos(psi)*sin(theta)*sin(phi);
+   M[1][0] = -sin(psi)*cos(phi)+cos(psi)*sin(theta)*sin(phi); //OK druga kolumna pierwszy element
 
-   M[2][0] = sin(psi)*sin(phi)+cos(psi)*cos(phi)*sin(theta);
-
-
-
-   M[0][1] = sin(psi)*cos(theta);
-
-   M[1][1] = cos(psi)*cos(phi)+sin(psi)*sin(theta)*sin(phi);
-
-   M[2][1] = cos(theta)*sin(phi);
+   M[2][0] = sin(psi)*sin(phi)+cos(psi)*cos(phi)*sin(theta);  //OK trzecia kolumna pierwszy element
 
 
 
-   M[0][2] = -sin(theta);
+   M[0][1] = sin(psi)*cos(theta);                             //OK pierwsza kolumna drugi element
 
-   M[1][2] = -cos(psi)*sin(phi)+sin(psi)*sin(theta)*cos(phi);
+   M[1][1] = cos(psi)*cos(phi)+sin(psi)*sin(theta)*sin(phi);  //OK druga kolumna drugi element
 
-   M[2][2] = cos(theta)*cos(phi);
+   M[2][1] = -cos(psi)*sin(phi)+sin(psi)*sin(theta)*cos(phi);                             // OK druga kolumna, trzeci element
+
+
+
+   M[0][2] = -sin(theta);                                     // OK pierwsza kolumna trzeci element
+
+   M[1][2] =  cos(theta)*sin(phi);// NOK trzecia kolumna drugi element
+
+   M[2][2] = cos(theta)*cos(phi);                             // OK trzecia kolumna trzeci element
 
 }
 
@@ -369,6 +365,10 @@ unsigned char setPos(float pe[]){
 void loop()
 
 {
+  Serial.print("RPX:");
+  Serial.print((analogRead(X_pin)-506)*(0.12/506));
+  Serial.print("RPY:");
+  Serial.println((analogRead(Y_pin)-506)*(0.12/506));
 arr[0] = 0;
 arr[1] = 0;
 arr[2] = 0;
